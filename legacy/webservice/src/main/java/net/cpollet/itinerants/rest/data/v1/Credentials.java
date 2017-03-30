eacaba9711c62f14c3e843527ec490488e4f01cd
Christophe Pollet <cpollet@users.noreply.github.com>
2017-03-30 22:03:51 +0200

    Removed legacy

--- legacy/webservice/src/main/java/net/cpollet/itinerants/rest/data/v1/Credentials.java
+++
@@ -1,9 +0,0 @@
-package net.cpollet.itinerants.rest.data.v1;
-
-/**
- * @author Christophe Pollet
- */
-public class Credentials {
-    public String username;
-    public String password;
-}
--- legacy/webservice/src/main/webapp/index.jsp
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-<p><a href="api/v1/users/cpollet">User 'cpollet'</a>
-<p><a href="api/v1/users/unknown">User 'unknown'</a>
-</body>
-</html>
