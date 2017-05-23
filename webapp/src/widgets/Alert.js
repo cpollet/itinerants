1a1819da7772727e3579a9f5c03ca3d9394073f5
Christophe Pollet <cpollet@users.noreply.github.com>
2017-05-23 21:29:02 +0200

    Alert can use children instead of text attribute

--- webapp/src/widgets/Alert.js
+++ webapp/src/widgets/Alert.js
@@ -7,14 +7,15 @@ class Alert extends React.Component {
             <div className={styles.component + ' ' + styles[this.props.type]}>
                 <strong><i className={styles.icon}/> {this.props.title}</strong>
                 &nbsp;
-                {this.props.text}
+                {this.props.text || this.props.children}
             </div>
         );
     }
 }
 
 Alert.propTypes = {
-    text: React.PropTypes.string.isRequired,
+    text: React.PropTypes.string,
+    children: React.PropTypes.node,
     title: React.PropTypes.string,
     type: React.PropTypes.oneOf(['success', 'info', 'warn', 'error']).isRequired,
 };
