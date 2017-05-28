07c634479ecf2b735fdc33e5dfb0acc70101a6c3
Christophe Pollet <cpollet@users.noreply.github.com>
2017-05-28 09:08:00 +0200

    fixed for past events as well

--- webservice/da/src/main/java/net/cpollet/itinerants/da/neo4j/repositories/EventRepository.java
+++ webservice/da/src/main/java/net/cpollet/itinerants/da/neo4j/repositories/EventRepository.java
@@ -24,15 +24,11 @@ public interface EventRepository extends GraphRepository<Neo4JEventData> {
             "RETURN e, p, path")
     List<Neo4JEventData> future(@Param("timestamp") long timestamp, @Param("username") String username, Sort sort);
 
-    @Query("MATCH (e:Event), (p), path=(e)-[*0..1]-(p) " +
+    @Query("MATCH (e:Event) " +
             "WHERE e.timestamp < {timestamp} " +
-            "RETURN path")
+            "OPTIONAL MATCH path=(e)-[]-(p:Person) " +
+            "RETURN e, p, path")
     List<Neo4JEventData> past(@Param("timestamp") long timestamp, Sort sort);
-
-    @Query("MATCH (e:Event), (p), path=(e)-[*0..1]-(p) " +
-            "WHERE e.timestamp < {timestamp} AND p.username = {username} " +
-            "RETURN path")
-    List<Neo4JEventData> past(@Param("timestamp") long timestamp, @Param("username") String username, Sort sort);
 
     Neo4JEventData findOneByUUID(@Param("uuid") String uuid);
 
